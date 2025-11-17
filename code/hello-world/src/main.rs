a2e0aca1456f94db376f921b7e577f6852b0eda4
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-11-17 12:15:25 +0100

    notebook 01 - done

--- code/hello-world/src/main.rs
+++ code/hello-world/src/main.rs
@@ -1,7 +1,33 @@
+const GLOBAL_NUMBER: u32 = 42;
+const GLOBAL_TEXT: &str = "Global Text";
+
+static MESSAGE: &str = "Rust 2024";
+
+static mut COUNTER: u32 = 0;
+
 fn main() {
+    unsafe {
+        COUNTER += 1;
+    }
+    
+    let local_counter;
+    unsafe {
+        local_counter = COUNTER;
+    }
+
+    println!("{MESSAGE} - {GLOBAL_NUMBER} - {local_counter}");
+    
     let text= "Hello, world!";
 
     println!("Text: {} - length: {}", text, text.len());
+
+    let mut number = 42; // int number = 42;
+    number += 1;
+    number = std::i32::MAX;
+    println!("number: {number}");
+    
+    let result = number.checked_add(1);
+    println!("result: {result:?}");
 }
 
 #[cfg(test)]
